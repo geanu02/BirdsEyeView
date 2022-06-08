fb537d4393d688b54dbe702a0e20f7e0b13646c4
Gian Carlo Torres <geanu@outlook.com>
2022-06-07 20:46:14 -0400

    apiConfig import fixed

--- src/context/mainContext.js
+++ src/context/mainContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from "react"
-import apiConfig from "../utils/config.js"
+import apiConfig from "../utils/config"
 
 const MainContext = createContext()
 
